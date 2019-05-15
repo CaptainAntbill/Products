46e872420fc2372cffb69a75d8dbbd875304540d
Anthony Bill <mejiaanthony016@gmail.com>
2019-05-15 11:47:50 -0600

    Creacion del metodo delete

---
+++ delete.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="css/bulma.css">
+</head>
+<body>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="ingreso.html">
+          Inicio
+        </a>
+
+        <a class="navbar-item" href="list_products.php">
+          Productos
+        </a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+        </div>
+      </div>
+    </div>
+  </nav><br>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "1234";
+  $dbname = "products";
+
+  $id = $_GET["id"];
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, '1234');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::
+    ERRMODE_EXCEPTION);
+    $sql = "DELETE FROM product where id=$id;";
+    $conn->exec($sql);
+    echo  "<label class='title' style='text-align: center'>Se he ingresado correctamente!</label><br><br>";
+  }
+  catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  $conn = null;
+  header("Location: list_products.php ")
+  ?>
+</body>
+</html>
