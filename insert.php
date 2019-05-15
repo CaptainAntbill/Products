5590eb533e182044465c443ba8a68592aee4421f
Anthony Bill <mejiaanthony016@gmail.com>
2019-05-15 11:46:07 -0600

    Modificamos el insert.php

--- insert.php
+++ insert.php
@@ -1,61 +1,63 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="css/bulma.css">
-  </head>
-  <body>
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-    </a>
-
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="ingreso.html">
-        Inicio
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="css/bulma.css">
+</head>
+<body>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
       </a>
 
-      <a class="navbar-item" href="list_products.php">
-        Productos
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
       </a>
     </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="ingreso.html">
+          Inicio
+        </a>
+
+        <a class="navbar-item" href="list_products.php">
+          Productos
+        </a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+        </div>
       </div>
     </div>
-  </div>
-</nav><br>
-    <?php
-      $servername = "localhost";
-      $username = "root";
-      $password = "1234";
-      $dbname = "products";
+  </nav><br>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "1234";
+  $dbname = "products";
 
-      $product_name = $_POST["product"];
-      $price = $_POST["price"];
+  $product_name = $_POST["product"];
+  $price = $_POST["price"];
+  $is_active = $_POST["is_active"];
 
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, '1234');
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::
-            ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO product VALUES (null, '$product_name', '$price', true )";
-        $conn->exec($sql);
-        echo  "<label class='title' style='text-align: center'>Se he ingresado correctamente!</label><br><br>";
-        echo "<a class='button is-link is-rounded' href='ingreso.html'>Regresar</a>";
-      }
-      catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-      }
-      $conn = null;
-     ?>
-  </body>
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, '1234');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::
+    ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO product VALUES (null, '$product_name', '$price', '$is_active')";
+    $conn->exec($sql);
+    echo  "<label class='title' style='text-align: center'>Se he ingresado correctamente!</label><br><br>";
+
+  }
+  catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  $conn = null;
+  header("Location: list_products.php");
+  ?>
+</body>
 </html>
